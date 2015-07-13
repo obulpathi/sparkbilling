5926c6a2770e487ff4cbc90d4e8b82f7219c7207
Obulpathi <obulapathi.challa@rackspace.com>
2015-07-13 11:45:27 -0400

    Aggregate logs

--- process1.py
+++ process1.py
@@ -17,15 +17,44 @@ def formatLogLine(record):
     tokens = record.split("\t")
     url = tokens[4]
     m = re.search('/(.+?).raxcdn.com/', url)
-    tokens.append(countryMap.value[tokens[11]])
-    return (m.group(1), tokens)
+    domain = m.group(1)
+    bandwidth = tokens[6]
+    region = countryMap.value[tokens[11]]
+    return (domain, (domain, region, int(bandwidth)))
+
+# create a combiner
+def createCombiner((domain, region, bandwidth)):
+    bw = {
+        "EMEA": 0,
+        "APAC": 0,
+        "North America": 0,
+        "South America": 0,
+        "Japan": 0,
+        "India": 0
+    }
+    bw[region] = bandwidth
+    return (domain, bw)
+
+# merge a value
+def mergeValue((domain, bw), (domain1, region, bandwidth)):
+    bw[region] = bw[region] + bandwidth
+    return (domain, bw)
+
+# merge two combiners: domain = domain1
+def mergeCombiners((domain, bw1), (domain1, bw2)):
+    for region in bw1:
+        bw1[region] = bw1[region] + bw2[region]
+    return (domain, bw1)
 
 def process(master, input_container, output_container):
-    sc = SparkContext(master, "CDNBilling")
+    import re
 
-    # Anantha
-    countryMap = sc.broadcast(sc.textFile(input_container + "/country_map.tsv"))
-    domainMap = sc.textFile(input_container + "/domain_map.tsv")
+    # load broadcast variables
+    countryMap = sc.broadcast(loadCountryMap())
+
+    # load domain map: Ananta
+    domain_map = load domains map
+    # domainMap.foreach(myprint)
 
     # load logs
     logsRDD = sc.textFile("sample.log")
@@ -33,13 +62,10 @@ def process(master, input_container, output_container):
     filteredRDD = logsRDD.filter(lambda x: x[0] != '#')
     # format the data
     formattedRDD = filteredRDD.map(formatLogLine, countryMap)
-
-
-
-
-    domainMap.foreach(myprint)
-    # Obulpathi
-    aggregatedLogs = domainLogs.reduceByKey(accumalator)
+    # for each domain, calculate bandwidth and request count
+    aggregatedLogs = formattedRDD.combineByKey(createCombiner, mergeValue, mergeCombiners)
+    # print the data
+    aggregatedLogs.foreach(myprint)
 
     joinedLogs = aggregaredLogs.join()
 
