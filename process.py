b5914a50c5dd88780a105f935760f1314e1178d6
Anantha Arunachalam <anantha.arunachalam@rackspace.com>
2015-07-13 18:54:11 -0400

    Spark Application

--- process.py
+++ process.py
@@ -1,5 +1,8 @@
 from pyspark import SparkContext
 import re
+import time, datetime
+
+countryMapDict = {}
 
 def myprint(text):
     print(text)
@@ -10,7 +13,7 @@ def formatLogLine(record):
     m = re.search('/(.+?).raxcdn.com/', url)
     domain = m.group(1)
     bandwidth = tokens[6]
-    region = countryMap.value.get(tokens[11], 'None')
+    region = countryMapDict.get(tokens[11], 'None')
     return (domain, (domain, region, int(bandwidth)))
 
 def formatDomainsLine(record):
@@ -43,13 +46,26 @@ def mergeCombiners((domain, bw1), (domain1, bw2)):
         bw1[region] = bw1[region] + bw2[region]
     return (domain, bw1)
 
+def createCountryDict(list):
+    country_list = []
+    for item in list:
+        tokens = item.split("\t")
+        country_list.append((tokens[0],tokens[1]))
+    return dict(country_list)
+
+def get_time():
+    timestamp = time.time()
+    time_formatted = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    return time_formatted
+
 def process(master, input_container, output_container):
     sc = SparkContext(master, "CDNBilling")
 
-    import re
-
     # load broadcast variables
-    countryMap = sc.broadcast(loadCountryMap())
+    countryMapRDD = sc.textFile(input_container + "/country_map.tsv")
+    countryMapList = countryMapRDD.collect()
+    countryMap = sc.broadcast(countryMapList)
+    countryMapDict.update(createCountryDict(countryMapList))
 
     # load domainLogs
     domainsRawRDD = sc.textFile(input_container + "/domains_map.tsv")
@@ -63,16 +79,24 @@ def process(master, input_container, output_container):
     # the above two steps can be optimized into a single step using
     # wholeFilesRDD?
     # format the data
-    formattedRDD = filteredRDD.map(formatLogLine, countryMap)
+    formattedRDD = filteredRDD.map(formatLogLine, countryMapDict)
     # for each domain, calculate bandwidth and request count
     aggregatedLogs = formattedRDD.combineByKey(createCombiner, mergeValue, mergeCombiners)
 
     # join the usage logs with domains map
     joinedLogs = aggregatedLogs.join(domainsRDD)
     # save the output
-    joinedLogs.saveAsTextFile("output")
+    joinedLogs.saveAsTextFile(output_container + "/output")
 
     sc.stop()
 
 if __name__ == "__main__":
-    process()
+    ## Execution : $YOUR_SPARK_HOME/bin/spark-submit --master local[4] process.py
+    input_container = "/home/nithya/spark-billing/sparkbilling"
+    output_container = "/home/nithya/spark-billing/sparkbilling"
+    f = open(output_container + "/time_taken.txt", 'w')
+    f.write("Start time: " + get_time() + "\n")
+    process("local", input_container, output_container)
+    f.write("End time: " + get_time() + "\n")
+    f.close()
+
